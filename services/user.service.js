344f86e3f80da4594360cc94ba6ade0011e5487c
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-17 18:47:35 -0500

    MASTER: Add create, update and delete with the ORM

--- services/user.service.js
+++ services/user.service.js
@@ -1,30 +1,38 @@
 const { models } = require('./../libs/sequelize');
+const boom = require('@hapi/boom');
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    const result = await models.User.findAll();
-    return result;
+    const users = await models.User.findAll();
+    return users;
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound('User not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const result = await user.update(changes);
+
+    return result;
   }
 
   async delete(id) {
-    return { id };
+    const user = await this.findOne(id);
+    await user.destroy();
+    return true;
   }
 }
 
