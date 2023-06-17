e1fb9d1e851df844b75f18ef428962a36fa30aa9
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-17 16:39:40 -0500

    MASTER: Add Postgres pool

--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,21 @@
-// const boom = require('@hapi/boom');
-
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => {
+      console.error('Unexpected error on idle client', err);
+    });
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const query = await client.query('SELECT * FROM tasks');
-    return query.rows;
+    const query = 'SELECT * FROM tasks';
+    const result = await this.pool.query(query);
+    return result.rows;
   }
 
   async findOne(id) {
