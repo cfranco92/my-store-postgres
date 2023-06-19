108ae82efb1030b52635e48a33166a5e71da533e
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-18 19:16:14 -0500

    MASTER: Add new column to database and create new migration

--- db/migrations/20230619001503-create-user.js
+++ db/migrations/20230619001503-create-user.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { UserChema, USER_TABLE } = require('../models/user.model');
+const { UserSchema, USER_TABLE } = require('../models/user.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserChema);
+    await queryInterface.createTable(USER_TABLE, UserSchema);
   },
 
   async down(queryInterface) {
