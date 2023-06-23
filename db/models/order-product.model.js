46d2a5a5717f4df3134b088d689ae20cba21e3de
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-23 16:07:01 -0500

    MASTER: Add order-product table

---
+++ db/models/order-product.model.js
@@ -0,0 +1,74 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const { ORDER_TABLE } = require('./order.model');
+const { PRODUCT_TABLE } = require('./product.model');
+
+const ORDER_PRODUCT_TABLE = 'orders_products';
+
+const OrderProductSchema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    field: 'created_at',
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  ammount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  orderId: {
+    field: 'order_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: ORDER_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+  productId: {
+    field: 'product_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: PRODUCT_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+};
+
+class OrderProduct extends Model {
+  static associate(models) {
+    this.belongsTo(models.Order, {
+      foreignKey: 'orderId',
+      as: 'order',
+    });
+    this.belongsTo(models.Product, {
+      foreignKey: 'productId',
+      as: 'product',
+    });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: ORDER_PRODUCT_TABLE,
+      modelName: 'OrderProduct',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = {
+  OrderProduct,
+  OrderProductSchema,
+  ORDER_PRODUCT_TABLE,
+};
