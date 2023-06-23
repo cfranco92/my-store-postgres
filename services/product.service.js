92f24039d06d4bf0f0c5758a42f1b6053cce8ff1
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-23 18:50:08 -0500

    MASTER: Add price filter

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker');
+const { Op } = require('sequelize');
 const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
 
@@ -28,11 +29,20 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where: {},
     };
     const { limit, offset } = query;
     if (limit && offset) {
       options.limit = limit;
       options.offset = offset;
+    }
+
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
     }
     const products = await models.Product.findAll(options);
     return products;
